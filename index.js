32deeaedc0396f0d96675caba761397c51f3e7bc
August <auggierutter@gmail.com>
2021-06-19 11:04:00 -0400

    commit tree 478f760527653b8e37fd39fa47b95d9048d37f5e

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+
+function submitData(name,email){
+const body = document.getElementsByTagName('body')
+const id = document.getElementsByClassName('id')
+const div = document.createElement('div')
+
+const meal = {
+    name: 'Steve',
+    email: 'steve@steve.com'}
+const config =  {
+    method:'POST',
+    headers: {
+        "Content-type": "application/json",
+        "Accept": "application/json"},
+        body: JSON.stringify(meal)}
+
+    return fetch('http://localhost:3000/users',config)
+        .then((response) =>response.json())
+        .then((object)=>{
+        const data = object.id
+            document.body.appendChild(div).append(data)})
+        .catch(function(error){ document.body.appendChild(div).append(error.message)
+        })
+}
